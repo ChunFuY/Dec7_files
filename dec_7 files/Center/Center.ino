23d5ac69cd3843bc9c66cc68170d03d25b7daf83
Spidre <louistaing@aol.com>
2022-12-08 13:43:20 -0800

    Update Center.ino

--- dec_7 files/Center/Center.ino
+++ dec_7 files/Center/Center.ino
@@ -1,6 +1,19 @@
 #include <SoftwareSerial.h>
 SoftwareSerial s(2,3);
 #include <Wire.h>
+
+//stepper motor
+#include <Stepper.h>
+const int stepsPerRevolution = 400;  // change this to fit the number of steps per revolution
+// for your motor
+
+//set up motor
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 7, 9,8, 10);
+int startLocation = 1;
+int CW = 0;
+int CCW = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(9);
@@ -8,6 +21,9 @@ void setup() {
   Wire.onRequest(RequestEvent);
   s.begin(115200);
   Serial.begin(921600);
+
+  //set up motor speed
+  myStepper.setSpeed(20);
 }
 void RequestEvent()
 {
@@ -24,6 +40,27 @@ void ReceiveEvent()
   {
     Serial.write(Wire.read());
   }
+}
+
+void motorCW()
+{
+    // step one revolution  in one direction:
+    Serial.println("clockwise");
+    CW = (stepsPerRevolution);
+    myStepper.step(CW);
+    delay(1000);
+    Serial.println("at location A");
+
+}
+
+void motorCCW()
+{
+  // step one revolution  in the other direction:
+    Serial.println("counterclockwise");
+    CCW = (-stepsPerRevolution);
+    myStepper.step(CCW);
+    delay(1000);
+    Serial.println("at location B");
 }
 void loop() {
   // put your main code here, to run repeatedly:
